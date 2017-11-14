bc260a28a5402a330e9f25659cbec74b2c9817cd
JustineBoulanger <bouljujube@gmail.com>
2017-11-14 15:08:54 -0500

    Module serveur Saas Outil meta-sql Module projet

---
+++ Serveur/outils/meta_sql/meta_sql.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import os,os.path
+import sys
+import Pyro4
+import socket
+from subprocess import Popen 
+import math
+from meta_sql_vue import *
+from IdMaker import Id
+
+class Controleur():
+    def __init__(self):
+        print("Controleur")
+        self.createurId=Id
+        self.modele=None
+        self.vue=Vue(self)
+        self.vue.root.mainloop()
+        
+        
+    
+if __name__ == '__main__':
+    c=Controleur()
