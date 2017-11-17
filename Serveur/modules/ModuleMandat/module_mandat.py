37b36d9285b2eeb359d604ba40eea5ab220a383b
William Tang <williamtang1003@gmail.com>
2017-11-17 11:04:19 -0500

    pour pull

--- Serveur/modules/ModuleMandat/module_mandat.py
+++ Serveur/modules/ModuleMandat/module_mandat.py
@@ -29,6 +29,13 @@ class Vue():
         self.menubar.add_command(label="Charger un fichier", command= lambda: self.parent.modele.explorateurFichiers(self.text))
         self.root.config(menu=self.menubar)
         
+    def choisirMot(self,event):
+        start = self.text.index('@%s,%s wordstart' % (event.x, event.y))
+        stop = self.text.index('@%s,%s wordend' % (event.x, event.y))
+        mot = repr(self.text.get(start, stop))
+        print(mot)
+        
+    
     def ecranMandat(self):
         self.frameMandat = Frame(self.fenetre, width = self.largeurMandat, height=self.hauteurMandat, bg="steelblue", relief=RAISED, padx=10, pady=10)
         self.frameMandat.pack()
@@ -37,8 +44,7 @@ class Vue():
         self.text.insert("%d.%d" %(0,1),texteInitial)
         self.text.bind("<Button-1>",self.tagging)
         self.text.pack()
-        
-        
+           
     def ecranCommande(self):
         self.frameCommande=Frame(self.fenetre, width=self.largeurMandat, height=self.hauteurTotale/2, bg="steelblue", padx=10,pady=10)
         self.frameCommande.pack(fill=X)
@@ -48,6 +54,7 @@ class Vue():
         #self.lblExpression=Label(self.frameCommande, text="Expression:", padx=30, pady=10, bg="light gray")
         #self.lblExpression.pack(side=LEFT)
         self.tfExpression=Entry(self.canCommande, width=100)
+        self.tfExpression.insert(0, "Cliquer sur un mot")
         self.canCommande.create_window(400,30,window=self.tfExpression,width=600,height=20)
         
         self.btnObjet=Button(self.frameCommande, text="Objet", width=30)
@@ -84,18 +91,17 @@ class Vue():
     def tagging(self,event):
         # get the index of the mouse click
         index = self.text.index("@%s,%s" % (event.x, event.y))
-
         # get the indices of all "adj" tags
         tag_indices = list(self.text.tag_ranges("jaune"))
         #enlever le tag "jaune" qui se trouve dans l'index choisi
-        #index2 = index+1
-        
+        #index2 = index+1 
         #self.text.tag_remove(str("jaune"),str(index),str(index+1))
          
         self.text.tag_add("jaune", "@%d,%d" % (event.x, event.y))   
-        #self.propagateTag(event)
+        self.propagateTag(event)
         self.specialEffect()
         self.parent.modele.ajouter(self.frameMandat)
+        self.choisirMot(event)
         
     def propagateTag(self, event):
         start = self.text.index('@%s,%s wordstart' % (event.x, event.y))
