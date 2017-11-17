0212b3c34a3f31a05a64fbeb2a43f194aaea06c4
Jessica <aaa>
2017-11-17 16:11:36 -0500

    Bonne version Cas Usage

--- Serveur/modules/CasUsage/ServeurBDcas.py
+++ Serveur/modules/CasUsage/ServeurBDcas.py
@@ -1,78 +1,95 @@
 import sqlite3
+from _overlapped import NULL
     
 class ServeurBDcas():
     def __init__(self,pControleur):
-        self.etat="NonTerminé"
+        self.Etat="NonTerminé"
         self.database = sqlite3.connect('cas.db')
         self.curseur = self.database.cursor()
-        self.curseur.execute("select * from client")
-        self.id = len(self.curseur.fetchall())
-        
+        self.curseur.execute("select * from casUsage")
+        self.Id = len(self.curseur.fetchall())
+        self.IdScenari=0
         
     def modifierCas(self,cas,usager,machine,indice):
-       
-        self.curseur.execute("UPDATE client SET cas=? WHERE id=?", (cas,indice,))
-        self.curseur.execute("UPDATE client SET usager=? WHERE id=?", (usager,indice,))
-        self.curseur.execute("UPDATE client SET machine=? WHERE id=?", (machine,indice,))
+
+        self.curseur.execute("UPDATE CasUsage SET Description=? WHERE Id=?", (cas,indice,))
+        self.curseur.execute("UPDATE ScenarioUtilisation SET Actions=? WHERE ScenarioUtilisation.Id=? and ScenarioUtilisation.ScenarioUtilisation_Id=(Select ScenarioUtilisation_Id from CasUsage)", (usager,1,))
+        self.curseur.execute("UPDATE ScenarioUtilisation SET Actions=? WHERE ScenarioUtilisation.Id=? and ScenarioUtilisation.ScenarioUtilisation_Id=(Select ScenarioUtilisation_Id from CasUsage)", (machine,2,))
         self.database.commit()
         
         
-    def envoyerCas(self,cas,usager,machine):
-        self.id+=1
-        self.curseur.execute("INSERT INTO client VALUES (?,?,?,?,?);", (self.id,cas, usager, machine,self.etat))
+    def envoyerCas(self,cas):
+        self.Id+=1
+        self.curseur.execute("INSERT INTO CasUsage VALUES (?,?,?,?);", (self.Id,cas,self.Etat,self.Id))
+        
+        self.database.commit()
+   
+    def envoyerScenari(self,utlisateur,machine):
+        self.IdScenari+=1
+        self.curseur.execute("INSERT INTO ScenarioUtilisation VALUES (?,?,?,?);", (self.IdScenari,utlisateur,NULL,self.Id))
+        self.curseur.execute("INSERT INTO ScenarioUtilisation VALUES (?,?,?,?);", (self.IdScenari+1,machine,NULL,self.Id))
+        self.IdScenari=0
         self.database.commit()
     
-    def chercherBdcas(self,id):
-        print("Id",id)
-        cas=self.curseur.execute("SELECT cas FROM client WHERE id=?", (id,))
-        print("cas",cas)
+    def chercherBdcas(self,Id):
+
+        cas=self.curseur.execute("SELECT Description FROM CasUsage WHERE Id=?", (Id,))
         return cas.fetchone()[0]
     
-    def chercherBdUtilisateur(self,id):
-        print("Id",id)
-        usager=self.curseur.execute("SELECT usager FROM client WHERE id=?", (id,))
-        return usager.fetchone()[0] 
+    def chercherBdUtilisateur(self,indice):
+        print("Indice",indice)
+        
+        usager=self.curseur.execute("SELECT Actions FROM ScenarioUtilisation,CasUsage WHERE ScenarioUtilisation.Id=?  and CasUsage.ScenarioUtilisation_Id=? and CasUsage.ScenarioUtilisation_Id=ScenarioUtilisation.ScenarioUtilisation_Id", (1,indice,))
+        
+        return usager.fetchone()[0]
     
-    def chercherBdMachine(self,id):
-        machine=self.curseur.execute("SELECT machine FROM client WHERE id=?", (id,))
+    def chercherBdMachine(self,indice):
+        machine=self.curseur.execute("SELECT Actions FROM ScenarioUtilisation,CasUsage WHERE ScenarioUtilisation.Id=?  and CasUsage.ScenarioUtilisation_Id=? and CasUsage.ScenarioUtilisation_Id=ScenarioUtilisation.ScenarioUtilisation_Id", (2,indice))
+        
         return machine.fetchone()[0] 
     
-    def changerEtat(self,etatInitial):
-        etat=self.curseur.execute('SELECT etat FROM client WHERE id=?',(etatInitial+1,))
-        etatCompare=etat.fetchone()[0]
-        if(etatCompare=="NonTerminé"):
-            self.curseur.execute("UPDATE client SET etat=? WHERE id=? AND etat=?", ("Terminé",etatInitial+1,"NonTerminé",))
-        elif(etatCompare=="Terminé"):
-            self.curseur.execute("UPDATE client SET etat=? WHERE id=? AND etat=?", ("NonTerminé",etatInitial+1,"Terminé",))
-        elif(etatCompare=="Reprendre"):
-            self.curseur.execute("UPDATE client SET etat=? WHERE id=? AND etat=?", ("NonTerminé",etatInitial+1,"Reprendre",))
+    def changerEtat(self,EtatInitial):
+        Etat=self.curseur.execute('SELECT Etat FROM CasUsage WHERE Id=?',(EtatInitial+1,))
+        EtatCompare=Etat.fetchone()[0]
+        if(EtatCompare=="NonTerminé"):
+            self.curseur.execute("UPDATE CasUsage SET Etat=? WHERE Id=? AND Etat=?", ("Terminé",EtatInitial+1,"NonTerminé",))
+        elif(EtatCompare=="Terminé"):
+            self.curseur.execute("UPDATE CasUsage SET Etat=? WHERE Id=? AND Etat=?", ("NonTerminé",EtatInitial+1,"Terminé",))
+        elif(EtatCompare=="Reprendre"):
+            self.curseur.execute("UPDATE CasUsage SET Etat=? WHERE Id=? AND Etat=?", ("NonTerminé",EtatInitial+1,"Reprendre",))
             self.database.commit()
             return False
         self.database.commit()
     
-    def changerEtatReprendre(self,etatInitial):
-        etat=self.curseur.execute('SELECT etat FROM client WHERE id=?',(etatInitial+1,))
-        etatCompare=etat.fetchone()[0]
-        if(etatCompare=="NonTerminé"):
-            self.curseur.execute("UPDATE client SET etat=? WHERE id=? AND etat=?", ("Reprendre",etatInitial+1,"NonTerminé",))
-        elif(etatCompare=="Terminé"):
-            self.curseur.execute("UPDATE client SET etat=? WHERE id=? AND etat=?", ("Reprendre",etatInitial+1,"Terminé",))
+    def changerEtatReprendre(self,EtatInitial):
+        Etat=self.curseur.execute('SELECT Etat FROM CasUsage WHERE Id=?',(EtatInitial+1,))
+        EtatCompare=Etat.fetchone()[0]
+        if(EtatCompare=="NonTerminé"):
+            self.curseur.execute("UPDATE CasUsage SET Etat=? WHERE Id=? AND Etat=?", ("Reprendre",EtatInitial+1,"NonTerminé",))
+        elif(EtatCompare=="Terminé"):
+            self.curseur.execute("UPDATE CasUsage SET Etat=? WHERE Id=? AND Etat=?", ("Reprendre",EtatInitial+1,"Terminé",))
         self.database.commit()
         
         
     def remplirListeUsager(self):
         listeUsager=[]
-        for cas in self.curseur.execute('SELECT utilisateur FROM client'):
+        for cas in self.curseur.execute('SELECT Actions FROM ScenarioUtilisation Where Id=1'):
             listeUsager.append(cas)
-            
+        return listeUsager
+    def remplirListeMachine(self):
+        listeMachine=[]
+        for cas in self.curseur.execute('SELECT Actions FROM ScenarioUtilisation Where Id=2'):
+            listeMachine.append(cas)   
+        return listeUsager
+        
     def remplirListeCas(self):
         listeCas=[]
-        for cas in self.curseur.execute('SELECT cas FROM client'):
+        for cas in self.curseur.execute('SELECT Description FROM casUsage'):
             listeCas.append(cas)
         return listeCas
         
     def remplirListeEtat(self):
         listeEtat=[]
-        for etat in self.curseur.execute('SELECT etat FROM client'):
-            listeEtat.append(etat)
+        for Etat in self.curseur.execute('SELECT Etat FROM casUsage'):
+            listeEtat.append(Etat)
         return listeEtat
