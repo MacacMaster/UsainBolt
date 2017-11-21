3f2beb58b2d826eb1d73f685c3e9f3258adc3e38
William Tang <williamtang1003@gmail.com>
2017-11-21 14:59:38 -0500

    CRC vue menu terminé!! :)

--- Serveur/modules/CRC/CRC.py
+++ Serveur/modules/CRC/CRC.py
@@ -10,17 +10,91 @@ class Vue():
     def __init__(self, parent):
         self.parent=parent
         self.root=Tk() #Fenetre
-        self.root.title("MANDAT")
+        self.root.title("MODULE CRC")
         self.hauteurTotale=200
         self.largeurTotale=200
         self.hauteurMandat=200
         self.largeurMandat=200
         self.fenetre = Frame(master=self.root, width=self.largeurTotale, height=self.hauteurTotale, bg="steelblue")
         self.fenetre.pack()
+        self.creerVueMenu()
+        
     
+    def creerVueMenu(self):  
+        self.menu = Frame(self.fenetre, width = self.largeurMandat, height=self.hauteurMandat, bg="steelblue", relief=RAISED, padx=10, pady=10)
+        self.menu.pack(side=LEFT)
+        #self.menu.pack_propagate(0)
+        self.creerMenuGauche()
+        self.creerMenuDroite()
+        
+        
+        
+
+        
+    def creerMenuGauche(self):
+        self.menuGauche = Frame(self.menu, width = self.largeurMandat, height=self.hauteurMandat, bg="steelblue", relief=RAISED, padx=10, pady=10)
+        self.menuGauche.pack(side=LEFT)
+        
+        largeur = 25
+        
+        frame1 = Frame(self.menuGauche)
+        frame1.pack(fill=X, pady=5)
+        
+        lbl1 = Label(frame1, text="Liste des classes", width=largeur)
+        lbl1.pack(side=LEFT, padx=55, pady=5)           
+       
+        frame2 = Frame(self.menuGauche)
+        frame2.pack(fill=Y)
+        
+        listeClasses = Listbox(self.menuGauche, height=25)
+        listeClasses.pack(fill=Y)
+        
+        frame3 = Frame(self.menuGauche)
+        frame3.pack(fill=BOTH, expand=True, pady = 5)
+        
+        btnSuppression = Button(self.menuGauche, text = "Suppression")
+        btnSuppression.pack()
+        
     
-    def creerVueMenu(self):
-        self.menu = Frame(master=self.fenetre, width=500,height=800,bg="steelblue")
+    def creerMenuDroite(self):
+        self.menuDroite = Frame(self.menu, width = self.largeurMandat, height=self.hauteurMandat, bg="steelblue", relief=RAISED, padx=10, pady=10)
+        self.menuDroite.pack(side=LEFT)
+        
+        largeur = 25
+        
+        frame1 = Frame(self.menuDroite)
+        frame1.pack(fill=X, pady=5)
+        
+        lblTitre = Label(frame1, text="Informations", width=largeur)
+        lblTitre.pack(side=LEFT, padx=55, pady=5)           
+       
+        frame2 = Frame(self.menuDroite)
+        frame2.pack(fill=X)
+        
+        lblNomClasse = Label(frame2, text = "nom de la classe")
+        lblNomClasse.pack()
+        
+        lblProprietaire = Label(frame2, text = "propriétaire de la classe")
+        lblProprietaire.pack()
+        
+        lblResponsabilites = Label(frame2, text = "Responsabilités")
+        lblResponsabilites.pack()
+        
+        listeResponsabilites = Listbox(frame2)
+        listeResponsabilites.pack()
+              
+        lblCollaboration = Label(frame2, text = "Collaboration")
+        lblCollaboration.pack()
+        
+        listeCollaboration = Listbox(frame2)
+        listeCollaboration.pack()
+        
+        frame3 = Frame(self.menuDroite)
+        frame3.pack(fill=BOTH, expand=True, pady = 5)
+        
+        btnModification = Button(self.menuDroite, text = "Modification")
+        btnModification.pack()
+        
     
 class Modele():
     def __init__(self, parent):
